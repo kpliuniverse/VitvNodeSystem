5a5ca9d9703b50070383fb774a7f6b0c8e603282
kpliuniverse <120769397+kpliuniverse@users.noreply.github.com>
2024-06-20 14:22:57 +0800

    Update node.c

--- src/node.c
+++ src/node.c
@@ -1,1 +1,15 @@
-#include <stdio>
+
+#include <stdint.h>
+
+
+#define GRIDSIZE_T uint32_t
+struct GridInfo() {
+    void* ptr;
+    GRIDSIZE_T size;
+}
+
+GridInfo create_node_grid(GRIDSIZE_T size) {
+    void* x = calloc(size * size, 1)
+    return GridInfo;
+}
+
